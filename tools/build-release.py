469a88508450b3e041587d3c908db491950cc6fb
kmarton <kmarton@cloudera.com>
2020-06-05 15:51:04 +0200

    [YUNIKORN-167] Fixed release tool after helm chart related changes

--- tools/build-release.py
+++ tools/build-release.py
@@ -19,6 +19,7 @@ import os
 import shutil
 import git
 import tarfile
+import distutils.dir_util
 
 def build_release():
     tools_dir = os.path.dirname(os.path.realpath(__file__))
@@ -41,18 +42,15 @@ def build_release():
     release_base = os.path.join(staging_dir, release_package_name)
     release_top_path = os.path.join(os.path.dirname(tools_dir),
         "release-top-level-artifacts")
+    helm_chart_path = os.path.join(os.path.dirname(tools_dir),
+        "helm-charts")
 
     # setup artifacts in the relase base dir
-    setup_base_dir(release_top_path, release_base)
+    setup_base_dir(release_top_path, helm_chart_path, release_base)
 
     # download source code from github repo
     for repo_meta in repo_list:
         dowload_sourcecode(release_base, repo_meta)
-
-    # add helm chart templates
-    print("adding helm chart templates")
-    os.chdir(release_base)
-    os.symlink("k8shim/helm-charts", "helm-charts")
 
     # generate tarball
     tarball_name = release_package_name + ".tar.gz"
@@ -70,7 +68,7 @@ def exclude_files(tarinfo):
         return None
     return tarinfo
 
-def setup_base_dir(release_top_path, base_path):
+def setup_base_dir(release_top_path, helm_path, base_path):
     print("setting up base dir for release artifacts, path: %s" % base_path)
     if os.path.exists(base_path):
       raise Exception("staging dir %s already exist, please remove it and retry" % base_path)
@@ -83,6 +81,9 @@ def setup_base_dir(release_top_path, base_path):
         dest = os.path.join(base_path, file)
         print("copying files: %s ===> %s" % (org, dest))
         shutil.copy2(org, dest)
+    # copy helm charts
+    print("helm patch: %s, base path: %s", helm_path, base_path)
+    distutils.dir_util.copy_tree(helm_path, os.path.join(base_path, "helm-charts"))
 
 def dowload_sourcecode(base_path, repo_meta):
     print("downloading source code")
